9414a11d72259c2f742851d59e097335a9b6a798
CMBGamer2018 <kleinerchristian2006@gmail.com>
2021-06-15 18:57:28 +0200

    created client main

--- src/de/hexagonsoftware/cbec/Main.java
+++ src/de/hexagonsoftware/cbec/Main.java
@@ -5,8 +5,18 @@ import java.io.*;
 
 public class Main {
     private Socket clientSocket;
-    private PrintWriter out;
-    private BufferedReader in;
+    private static PrintWriter out;
+    private static BufferedReader in;
+
+    public static void main(String[] args) throws IOException {
+         Main main = new Main();
+         main.startConnection("84.188.211.238", 420);
+         while (true){
+             char[] buffer = new char[200];
+             int anzahlZeichen = in.read(buffer, 0, 200);
+             System.out.println(new String(buffer, 0, anzahlZeichen));
+         }
+    }
 
     public void startConnection(String ip, int port) throws IOException {
         clientSocket = new Socket(ip, port);
